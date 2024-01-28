0e2de19edb9e52949da6d678c2c41394bdd59cc1
belhadj haitam <steankir.haitam@gmail.com>
2024-01-28 15:43:25 +0100

    start

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,28 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 15:35:57 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/01/28 15:35:58 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/01/28 15:42:48 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+    private:
+        std::string Name;
+        int Hit_points;
+        int Energy_points;    
+        int Attack_damage;    
+    public:
+        ClapTrap(std::string Name);
+        ClapTrap(const ClapTrap& other);
+        ClapTrap& operator=(const ClapTrap& other);
+        ~ClapTrap();
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
