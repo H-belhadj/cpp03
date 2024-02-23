ec3bb2fc21474993465c689b51f2cadad1c0fa7b
belhadj haitam <steankir.haitam@gmail.com>
2024-02-23 19:01:04 +0100

    maybe

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 15:35:57 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/02/23 17:56:30 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/02/23 18:55:07 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ class ClapTrap
         int Energy_points;    
         int Attack_damage;    
     public:
+        ClapTrap();
         ClapTrap(std::string Name);
         ClapTrap(const ClapTrap& other);
         ClapTrap& operator=(const ClapTrap& other);
@@ -30,7 +31,7 @@ class ClapTrap
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
-        std::string get_Name(void) const;
+        // std::string get_Name(void) const;
 };
 
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,22 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 15:35:59 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/02/15 18:40:56 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/02/23 19:00:49 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int main(int argc, char **argv)
+int main()
 {
+    ClapTrap hicham("allouch"), tefa7i(hicham);
     
+    hicham.attack("messi");
+    tefa7i.attack("messi");
+    
+    hicham.takeDamage(55);
+    tefa7i.takeDamage(99);
+
+    hicham.beRepaired(1111111111);
+    tefa7i.beRepaired(2222222222);
 }
