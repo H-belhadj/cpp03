2dc8b49f860a370f022ea42ee4b2bee6e6c98e37
belhadj haitam <steankir.haitam@gmail.com>
2024-02-25 11:56:33 +0100

    last

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 11:19:58 by hbelhadj          #+#    #+#             */
-/*   Updated: 2024/02/24 11:41:17 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2024/02/25 11:56:16 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,28 @@
 
 int main() {
     // Create a ClapTrap object
-    ClapTrap clapTrap("CL4P-TP");
+    // ClapTrap clapTrap("CL4P-TP");
 
-    // Test ClapTrap functionalities
-    clapTrap.attack("Bandit");
-    clapTrap.takeDamage(20);
-    clapTrap.beRepaired(10);
+    // // Test ClapTrap functionalities
+    // clapTrap.attack("Bandit");
+    // clapTrap.takeDamage(20);
+    // clapTrap.beRepaired(10);
 
     // Create a FragTrap object
     FragTrap fragTrap("FR4G-TP");
 
     // Test FragTrap functionalities
-    // fragTrap.attack("Bandit");
-    // fragTrap.highFivesGuys();
-    // fragTrap.takeDamage(30);
+    fragTrap.attack("Bandit");
+    fragTrap.highFivesGuys();
+    fragTrap.takeDamage(30);
 
     // Create a ScavTrap object
-    ScavTrap scavTrap("SC4V-TP");
+    // ScavTrap scavTrap("SC4V-TP");
 
-    // Test ScavTrap functionalities
-    scavTrap.attack("Bandit");
-    scavTrap.guardgate();
-    scavTrap.takeDamage(40);
+    // // Test ScavTrap functionalities
+    // scavTrap.attack("Bandit");
+    // scavTrap.guardgate();
+    // scavTrap.takeDamage(40);
 
     return 0;
 }
